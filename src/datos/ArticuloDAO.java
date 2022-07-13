9d39077e399d67652ff67c6c1c793fa14a70fc25
javierurreabejarano <89669453+javierurreabejarano@users.noreply.github.com>
2022-07-13 17:30:21 -0500

    Versión 3.0.1 Acceso a datos: Articulo

--- src/datos/ArticuloDAO.java
+++ src/datos/ArticuloDAO.java
@@ -9,8 +9,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import database.Conexion;
-import datos.interfaces.CrudSimpleInterface;
-import entidades.Categoria;
+import datos.interfaces.CrudPaginadoInterface;
+import entidades.Articulo;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
@@ -19,7 +19,7 @@ import javax.swing.JOptionPane;
  *
  * @author Javier
  */
-public class CategoriaDAO implements CrudSimpleInterface<Categoria> {
+public class ArticuloDAO implements CrudPaginadoInterface<Articulo> {
     private final Conexion CON;
     private PreparedStatement ps;
     private ResultSet rs;
@@ -27,19 +27,21 @@ public class CategoriaDAO implements CrudSimpleInterface<Categoria> {
     
     
     
-    public CategoriaDAO() {
+    public ArticuloDAO() {
         CON = Conexion.getInstancia();
     }
 
     @Override
-    public List<Categoria> listar(String texto) {
-        List<Categoria> registros = new ArrayList();
+    public List<Articulo> listar(String texto,int totalPorPagina,int numPagina) {
+        List<Articulo> registros = new ArrayList();
         try {
-            ps = CON.conectar().prepareStatement("SELECT * FROM categoria WHERE nombre LIKE ?");
+            ps = CON.conectar().prepareStatement("SELECT a.id,a.categoria_id, c.nombre as categoria_nombre, a.codigo, a.nombre, a.precio_venta, a.stock, a.descripcion, a.imagen, a.activo FROM articulo a inner join categoria c ON a.categoria_id=c.id WHERE a.nombre LIKE ? ORDER BY a.id ASC LIMIT ?,?");
             ps.setString(1, "%" + texto + "%");
+            ps.setInt(2, (numPagina-1)*totalPorPagina);
+            ps.setInt(3, totalPorPagina);
             rs = ps.executeQuery();
             while(rs.next()){
-                registros.add(new Categoria(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getBoolean(4)));
+                registros.add(new Articulo(rs.getInt(1),rs.getInt(2), rs.getString(3),rs.getString(4), rs.getString(5),rs.getDouble(6),rs.getInt(7),rs.getString(8),rs.getString(9), rs.getBoolean(10)));
             }
             ps.close();
             rs.close();
@@ -54,12 +56,17 @@ public class CategoriaDAO implements CrudSimpleInterface<Categoria> {
     }
 
     @Override
-    public boolean insertar(Categoria obj) {
+    public boolean insertar(Articulo obj) {
         resp = false;
         try {
-            ps=CON.conectar().prepareStatement("INSERT INTO categoria (nombre,descripcion,activo) VALUES (?,?,1)");
-            ps.setString(1, obj.getNombre());
-            ps.setString(2, obj.getDescripcion());
+            ps=CON.conectar().prepareStatement("INSERT INTO articulo (categoria_id,codigo,nombre,precio de venta,stock,descripcion,imagen,activo) VALUES (?,?,?,?,?,?,?,1)");
+            ps.setInt(1, obj.getCategoriaId());
+            ps.setString(2, obj.getCodigo());
+            ps.setString(3, obj.getNombre());
+            ps.setDouble(4, obj.getPrecioVenta());
+            ps.setInt(5, obj.getStock());
+            ps.setString(6, obj.getDescripcion());
+            ps.setString(7, obj.getImagen());
             if (ps.executeUpdate()>0){
                 resp=true;
             }
@@ -74,13 +81,18 @@ public class CategoriaDAO implements CrudSimpleInterface<Categoria> {
     }
 
     @Override
-    public boolean actualizar(Categoria obj) {
+    public boolean actualizar(Articulo obj) {
         resp = false;
         try {
-            ps = CON.conectar().prepareStatement("UPDATE categoria SET nombre=?, descripcion=? WHERE id=?");
-            ps.setString(1, obj.getNombre());
-            ps.setString(2, obj.getDescripcion());
-            ps.setInt(3, obj.getId());
+            ps = CON.conectar().prepareStatement("UPDATE articulo SET categoria_id=?, codigo=?, nombre=?, precio_venta=?, stock=?, descripcion=?, imagen=? WHERE id=?");
+            ps.setInt(1, obj.getCategoriaId());
+            ps.setString(2, obj.getCodigo());
+            ps.setString(3, obj.getNombre());
+            ps.setDouble(4, obj.getPrecioVenta());
+            ps.setInt(5, obj.getStock());
+            ps.setString(6, obj.getDescripcion());
+            ps.setString(7, obj.getImagen());
+            ps.setInt(8, obj.getId());
             if (ps.executeUpdate()>0){
                    resp = true;
             }
@@ -98,7 +110,7 @@ public class CategoriaDAO implements CrudSimpleInterface<Categoria> {
     public boolean desactivar(int id) {
         resp = false;
         try {
-            ps = CON.conectar().prepareStatement("UPDATE categoria SET activo=0 WHERE id=?");
+            ps = CON.conectar().prepareStatement("UPDATE articulo SET activo=0 WHERE id=?");
             ps.setInt(1, id);
             if (ps.executeUpdate()>0){
                    resp = true;
@@ -117,7 +129,7 @@ public class CategoriaDAO implements CrudSimpleInterface<Categoria> {
     public boolean activar(int id) {
         resp = false;
         try {
-            ps = CON.conectar().prepareStatement("UPDATE categoria SET activo=1 WHERE id=?");
+            ps = CON.conectar().prepareStatement("UPDATE articulo SET activo=1 WHERE id=?");
             ps.setInt(1, id);
             if (ps.executeUpdate()>0){
                    resp = true;
@@ -136,7 +148,7 @@ public class CategoriaDAO implements CrudSimpleInterface<Categoria> {
     public int total() {
         int totalRegistros = 0;
         try {
-            ps = CON.conectar().prepareStatement("SELECT COUNT(id) FROM categoria");
+            ps = CON.conectar().prepareStatement("SELECT COUNT(id) FROM articulo");
             rs = ps.executeQuery();
             
             while(rs.next()){
@@ -158,7 +170,7 @@ public class CategoriaDAO implements CrudSimpleInterface<Categoria> {
     public boolean existe(String texto) {
         resp = true;
         try {
-            ps = CON.conectar().prepareStatement("SELECT nombre FROM categoria WHERE nombre=?");
+            ps = CON.conectar().prepareStatement("SELECT nombre FROM articulo WHERE nombre=?");
             ps.setString(1, texto);
             rs = ps.executeQuery();
             rs.last();
